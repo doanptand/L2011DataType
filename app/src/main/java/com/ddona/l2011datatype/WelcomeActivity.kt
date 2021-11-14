2e2496548f5271fee40cd82f93373a6f75bc5f68
DoanPT1 <doanptand@gmail.com>
2021-11-14 20:38:07 +0700

    call method from activity

--- app/src/main/java/com/ddona/l2011datatype/WelcomeActivity.kt
+++ app/src/main/java/com/ddona/l2011datatype/WelcomeActivity.kt
@@ -2,10 +2,43 @@ package com.ddona.l2011datatype
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.ddona.l2011datatype.databinding.ActivityWelcomeBinding
+import com.ddona.l2011datatype.db.StudentManager
+import com.ddona.l2011datatype.model.Student
 
 class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val studentManager = StudentManager(this)
+
+        binding.btnAdd.setOnClickListener {
+            val name = binding.edtName.text.toString()
+            val age = binding.edtAge.text.toString()
+            val className = binding.edtClassName.text.toString()
+            studentManager.addStudent2(
+                Student(
+                    name = name,
+                    age = age.toInt(),
+                    className = className
+                )
+            )
+        }
+        binding.btnDelete.setOnClickListener {
+            val id = binding.edtId.text.toString()
+            studentManager.deleteStudentById2(id.toInt())
+        }
+        binding.btnEdit.setOnClickListener {
+            val id = binding.edtId.text.toString()
+            val name = binding.edtName.text.toString()
+            val age = binding.edtAge.text.toString()
+            val className = binding.edtClassName.text.toString()
+            studentManager.updateStudent2(Student(id.toInt(), name, age.toInt(), className))
+        }
+        val students = studentManager.getAllStudent2()
+        Log.d("doanpt", "All student:${students.size}")
     }
 }
