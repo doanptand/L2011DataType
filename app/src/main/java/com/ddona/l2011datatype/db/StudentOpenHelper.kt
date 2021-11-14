9d1b51ce9834e7baaa9796fafe480e8eda19cca7
DoanPT1 <doanptand@gmail.com>
2021-11-14 19:26:22 +0700

    Create sql open helper

---
+++ app/src/main/java/com/ddona/l2011datatype/db/StudentOpenHelper.kt
@@ -0,0 +1,18 @@
+package com.ddona.l2011datatype.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.ddona.l2011datatype.util.Const
+
+class StudentOpenHelper(context: Context) :
+    SQLiteOpenHelper(context, "student.db", null, 1) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(Const.QUERY_CREATE_STUDENT_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        
+    }
+}
---
+++ app/src/main/java/com/ddona/l2011datatype/model/Student.kt
@@ -0,0 +1,8 @@
+package com.ddona.l2011datatype.model
+
+data class Student(
+    var id: Int = 0,
+    var name: String = "",
+    var age: Int = 0,
+    var className: String = ""
+)
